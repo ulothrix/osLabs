306e4e5c9fce0c49b51cb3543a36c8c1ff0efbd0
Serhat <zerhat@gmail.com>
2018-03-13 19:19:08 +0300

    Add files via upload

---
+++ lab1/lab1B.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <memory.h>
+
+#define USERNAME 152120161094
+#define PASSWORD "asdf"
+
+char ogrNo[12];
+char sifre[4];
+
+void check();
+
+void main(){
+    printf("Ogrenci Numaranizi Giriniz: ");
+    scanf("%s",ogrNo);
+    printf("Sifrenizi Giriniz: ");
+    scanf("%s",sifre);
+    check();
+}
+void check(){
+    char temp[12];
+    sprintf(temp,"%ld", USERNAME);
+    if(strcmp(temp,ogrNo) == 0){
+        if(strcmp(sifre,PASSWORD) != 0){
+            printf("You Shall Not Pass ! \n");
+        }else if(strcmp(sifre,PASSWORD) == 0){
+            printf("Giris yapildi... \n");
+        }
+    }else{
+        printf("Bu ogrencinin yetkisi yok! \n");
+    }
+}
+
+
+
+
+
